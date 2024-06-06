694c9d49139e6bb7c197ba077b728d5cd2a942af
Nattapong <65160038@go.buu.ac.th>
2024-06-06 15:39:01 +0700

    add file

--- Day2/Project/src/components/todoComponent.vue
+++ Day2/Project/src/components/todoComponent.vue
@@ -1,10 +1,34 @@
 <script setup>
     import {ref, computed} from 'vue'
-    const todoList = ref(['Work1', 'Work2'])
+    const todoList = ref([{
+    id:1,
+    text: 'Work1',
+    status: true
+    }, 
+    {
+        id:2,
+        text: 'Work2',
+        status: true
+    }])
+
+    let lastId = 3
     const todo = ref('')
     function addTodo(){
-        todoList.value.push(todo.value)
+        todoList.value.push({
+            id: lastId,
+            text: todo.value,
+            status: true
+        })
+        lastId ++
         todo.value = ''
+    }
+
+    function done(index){
+        todoList.value[index].status  = !todoList.value[index].status
+    }
+
+    function del(index){
+        todoList.value.splice(index, 1)
     }
 </script>
 
@@ -12,7 +36,15 @@
     <div>
         <input type="text" v-model="todo"> <button @click="addTodo()">Add Todo</button>
         <ul>
-            <li v-for="(item, index) in todoList" :key="index">{{ index }} {{ item }}</li>
+            <li v-for="(item, index) in todoList" :key="index" @click="done(index)" :class="{finished: !item.status}">
+                {{ index }} {{ item.id }}.{{ item.text }} {{ item.status }} <button @click="del(index)">X</button>
+            </li>
         </ul>
     </div>
 </template>
+
+<style scoped>
+    .finished{
+        text-decoration: line-through;
+    }
+</style>
